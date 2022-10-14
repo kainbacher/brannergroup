7c0d8b1b51f2b97d99c4f153009bf3f28c693f42
kainbacher <rka@beautyparlour.at>
2022-10-14 09:05:48 +0200

    startpage

--- components/contact-card.vue
+++ components/contact-card.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex flex-col bg-white border border-gray-100 rounded-lg overflow-hidden">
-    <a href="#" class="group h-32 md:h-40 block bg-gray-100 overflow-hidden relative">
+    <a
+      :href="url"
+      :title="name"
+      class="group h-32 md:h-40 block bg-gray-100 overflow-hidden relative"
+      target="_blank"
+    >
       <img
         :src="image"
         loading="lazy"
@@ -9,37 +14,49 @@
       />
     </a>
 
-    <div class="flex flex-col flex-1 p-4 sm:p-6">
-      <h2 class="text-red-800 text-base font-semibold mb-2">
-        <a
-          href="#"
-          class="text-red-500 hover:text-red-700 transition duration-100"
-          >{{ name }}</a
-        >
-      </h2>
-      <slot></slot>
+    <div class="flex flex-col justify-between flex-1 p-4 sm:p-6">
+      <div>
+        <h2 class="text-red-800 text-base font-semibold mb-2">
+          <a href="#" class="text-red-500 hover:text-red-700 transition duration-100">{{
+            name
+          }}</a>
+        </h2>
+        <slot></slot>
+      </div>
+      <a
+        :href="url"
+        class="
+          bg-gray-50 border-gray-300 text-red-500 border rounded-md flex justify-center items-center py-1
+          hover:bg-red-50 hover:text-red-700 hover:border-red-300 transition duration-300
+        "
+        >weiter -></a
+      >
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Contact',
-    props: {
-      name: {
-        type: String,
-        default: '',
-      },
-      image: {
-        type: String,
-        default: '',
-      },
+export default {
+  name: "Contact",
+  props: {
+    name: {
+      type: String,
+      default: "",
     },
-    data() {
-      return {
-        posts: [],
-        loading: true,
-      }
+    image: {
+      type: String,
+      default: "",
     },
-  }
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      posts: [],
+      loading: true,
+    };
+  },
+};
 </script>
--- components/contact-list.vue
+++ components/contact-list.vue
@@ -5,63 +5,107 @@
         class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 md:gap-6 xl:gap-8"
       >
         <contact-card
-          name="Branner Autohaus"
-          image="http://branner.at/images/branner-fitness.jpg"
+          name="Branner Mobility"
+          image="/branner-mobility.jpg"
+          url="http://www.branner-autohaus.at/"
         >
-          <p class="text-gray-500 text-sm 2xl:text-base mb-8">
-            +43 (0) 5522 46118<br />
-            fitness@branner.at<br />
-            <br />
-            Alemannenstr. 21<br />
-            6830 Rankweil
-          </p>
+          <div>
+            <p class="text-gray-500 text-sm 2xl:text-base mb-8">
+              <a href="mailto:patrick@branner.at" title="E-Mail an Branner Mobility"
+                >patrick@branner.at</a
+              ><br />
+              <a href="tel:+43552357200" title="Bei Branner Mobility anrufen"
+                >+43 5523 57 200</a
+              ><br />
+              <br />
+              Treietstraße 2<br />
+              6833 Klaus
+            </p>
+            <div class="flex flex-row justify-between mb-4">
+              <a href="http://www.branner-autohaus.at/" target="_blank" title="Nissan">
+                <img src="/nissan.png" alt="Nissan" class="h-24 w-24" height="100" width="100" />
+              </a>
+              <a href="https://www.hofele.com/" target="_blank" title="Hofele">
+                <img src="/hofele.png" alt="Hofele" class="h-24 w-24" height="100" width="100" />
+              </a>
+            </div>
+          </div>
         </contact-card>
         <contact-card
           name="Branner Fitness"
-          image="http://branner.at/images/branner-autohaus.jpg"
+          image="/branner-fitness.jpg"
+          url="https://www.branner-fitness.at/"
         >
-          <p class="text-gray-500 text-sm 2xl:text-base mb-8">
-            +43 (0) 5522 46118<br />
-            fitness@branner.at<br />
+          <p class="text-gray-500 text-sm 2xl:text-base mb-4">
+            <a href="mailto:fitness@branner.at" title="E-Mail an Branner Fitness"
+              >fitness@branner.at</a
+            ><br />
             <br />
-            Alemannenstr. 21<br />
+            <a href="tel:+43552246118" title="Bei Branner Fitness anrufen"
+              >+43 5522 46 118</a
+            ><br />
+            Alemannenstraße 2<br />
             6830 Rankweil
+          </p>
+          <p class="text-gray-500 text-sm 2xl:text-base mb-8">
+            <a href="tel:+43552246118" title="Bei Branner Fitness anrufen"
+              >+43 5522 77 241</a
+            ><br />
+            Grenzweg 10<br />
+            6800 Feldkirch
           </p>
         </contact-card>
         <contact-card
           name="Branner Erdenwerk"
-          image="http://branner.at/images/branner-kompostierung.jpg"
+          image="/branner-erdenwerk.jpg"
+          url="https://www.branner-erdenwerk.at/"
         >
           <p class="text-gray-500 text-sm 2xl:text-base mb-8">
-            +43 (0) 5522 46118<br />
-            fitness@branner.at<br />
+            <a href="mailto:erdenwerk@branner.at" title="E-Mail an Branner Erdenwerk"
+              >erdenwerk@branner.at</a
+            ><br />
+            <a href="tel:+43552236644" title="Bei Branner ERdenwerk anrufen"
+              >+43 5522 366 44</a
+            ><br />
             <br />
-            Alemannenstr. 21<br />
+            Bundesstraße 120 2<br />
             6830 Rankweil
           </p>
         </contact-card>
         <contact-card
           name="Branner Entsorgung"
-          image="http://branner.at/images/branner-entsorgung.jpg"
+          image="/branner-entsorgung.jpg"
+          url="https://www.branner-entsorgung.at/"
         >
           <p class="text-gray-500 text-sm 2xl:text-base mb-8">
-            +43 (0) 5522 46118<br />
-            fitness@branner.at<br />
+            <a href="mailto:erdenwerk@branner.at" title="E-Mail an Branner Erdenwerk"
+              >erdenwerk@branner.at</a
+            ><br />
+            <a href="tel:+43552351141" title="Bei Branner ERdenwerk anrufen"
+              >+43 5523 511 41</a
+            ><br />
             <br />
-            Alemannenstr. 21<br />
-            6830 Rankweil
+            Treietstraße 2<br />
+            6833 Klaus
           </p>
         </contact-card>
         <contact-card
           name="Branner Oberflächenbehandlung"
-          image="http://branner.at/images/branner-oberflaechen.jpg"
+          image="/branner-oberflaechen.jpg"
+          url="https://www.branner-oberflaechen.at/"
         >
           <p class="text-gray-500 text-sm 2xl:text-base mb-8">
-            +43 (0) 5522 46118<br />
-            fitness@branner.at<br />
+            <a
+              href="mailto:dominik.pircher@branner.at"
+              title="E-Mail an Branner Erdenwerk"
+              >dominik.pircher@branner.at</a
+            ><br />
+            <a href="tel:+436602336065" title="Bei Branner ERdenwerk anrufen"
+              >+43 660 233 60 65</a
+            ><br />
             <br />
-            Alemannenstr. 21<br />
-            6830 Rankweil
+            Treietstraße 2<br />
+            6833 Klaus
           </p>
         </contact-card>
       </div>

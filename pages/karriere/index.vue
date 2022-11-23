508899ea89f7764aabb7a153f6f3664745e107af
kainbacher <rka@beautyparlour.at>
2022-11-23 12:52:21 +0100

    add email button

--- pages/karriere/index.vue
+++ pages/karriere/index.vue
@@ -5,10 +5,21 @@
       class="text-gray-600 body-font overflow-hidden pt-24 max-w-6xl ml-auto mr-auto"
     >
       <div class="container px-5 mb-16 border-b-b p4-4 pb-8 md:w-3/5 mx-auto">
-        <h1 class="sm:text-3xl text-3xl font-medium title-font text-gray-900 mb-8">
-          Stellenausscrhreibungen<br />
-          <span class="text-gray-500">Wir suchen dich!</span>
-        </h1>
+        <div class="mb-8 flex flex-row justify-between items-center">
+          <h1 class="sm:text-3xl text-3xl font-medium title-font text-gray-900 mb-2">
+            Stellenausscrhreibungen<br />
+            <span class="text-gray-500">Wir suchen dich!</span>
+          </h1>
+          <div>
+            <a
+              href="mailto:dispo@branner.at"
+              title="Initiativbewerbung per E-Mail"
+              class="bg-red-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-red-600 rounded text-base mt-4 md:mt-0"
+            >
+              Initiativbewerbung
+            </a>
+          </div>
+        </div>
         <jobs-list :amount="10" />
       </div>
     </section>
